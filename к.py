53d1139c60239c58fde5b9116971d145d89d2228
O.Alekseeva <olena9002@gmail.com>
2023-09-28 17:21:55 +0300

    merge conflicts

--- к.py
+++ к.py
@@ -1,13 +1,1 @@
-
-def wrapper(func):
-		def inner():
-				print(func.__name__)
-				str_ = func().split()
-				print(', '.join(str_))
-				return ', '.join(str_)
-
-		return inner()
-
-@wrapper
-def check():
-		return 'Check print'
+'THAT K'
