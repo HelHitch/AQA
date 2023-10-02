94e0e42b1c7a1de2cea67a176a817a441b3cfaea
HelHitch <olena9002@gmail.com>
2023-10-02 23:20:37 +0400

    Cutted out unused code lines

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@
 # -- coding: utf-8 --
 import pytest
 from selenium import webdriver
+from Pages.catalog import CatalogPage
 from Pages.log_in import LoginPage
 
 @pytest.fixture(scope='class')
@@ -11,11 +12,9 @@ def init_parameters():
     web_driver.close()
 
 
+
 @pytest.fixture(scope='class')
-def completed_auth(request, init_parameters):
-    CatalogPage = LoginPage(init_parameters)
-    CatalogPage = CatalogPage.login(username='standard_user', password='secret_sauce')
-    print('Into fixture')
-    yield CatalogPage
+def completed_auth(init_parameters):
+    yield LoginPage(init_parameters).login(username='standard_user', password='secret_sauce')
 
 
