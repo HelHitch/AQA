75d84ccd101c5d152e53a6a6a16fa336abd09f7a
HelHitch <olena9002@gmail.com>
2023-10-02 23:21:15 +0400

    Added description for files

--- Pages/catalog.py
+++ Pages/catalog.py
@@ -1,28 +1,23 @@
-
 from Pages.BasePage import BasePage
 from selenium.webdriver.common.by import By
 
 
 class CatalogPage(BasePage):
-		URL = "https://www.saucedemo.com/inventory.html"
+    URL = "https://www.saucedemo.com/inventory.html"
 
-		"""Page locators"""
-		CATALOG_DIV = (By.ID , "inventory_container")
-		ITEM_LOCATOR = (By.XPATH , "//button[contains(@class, 'btn_inventory')]")
-		CARD_LOCATOR = (By.XPATH , "//span[contains(@class, 'shopping_cart_badge')]")
+    """Page locators"""
+    CATALOG_DIV = (By.ID, "inventory_container")
+    ITEM_LOCATOR = (By.XPATH, "//button[contains(@class, 'btn_inventory')]")
+    CARD_LOCATOR = (By.XPATH, "//span[contains(@class, 'shopping_cart_badge')]")
 
-		def __init__(self, driver):
-				super().__init__(driver)
+    def __init__(self, driver):
+        super().__init__(driver)
 
-		"""Main functions"""
-		def add_element_to_card(self):
-				self.click(self.ITEM_LOCATOR)
+    """Main functions"""
 
-		def add_random_element_to_card(self):
-			self.click_random_element_from_list(self.ITEM_LOCATOR)
-			assert self.get_element_text(self.CARD_LOCATOR) == '1'
+    def add_element_to_card(self):
+        self.click(self.ITEM_LOCATOR)
 
-
-
-
-
+    def add_random_element_to_card(self):
+        self.click_random_element_from_list(self.ITEM_LOCATOR)
+        assert self.get_element_text(self.CARD_LOCATOR) == '1'
